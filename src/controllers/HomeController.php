60842197233d3a5eb48536a311d783ab30a70002
zulujive <51298470+zulujive@users.noreply.github.com>
2023-06-02 20:49:45 +0000

    Fixed routes

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -8,7 +8,6 @@ class HomeController
         require_once __DIR__ . '/../resources/views/home.php';
     }
     public function panelLogin() {
-        echo 'Executing panelLogin()'; // Add this line for testing purposes
-        // Rest of the code for handling the login logic
+        require_once __DIR__ . '/../resources/views/home.php';
     }
 }
