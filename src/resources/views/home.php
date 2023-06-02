d5b9b65c61380131e2e52ae02e46f9be1d0686ff
zulujive <51298470+zulujive@users.noreply.github.com>
2023-06-02 19:12:25 +0000

    Updated paths again

--- src/resources/views/home.php
+++ src/resources/views/home.php
@@ -2,7 +2,7 @@
 include(dirname(__FILE__).'/../../../config.php');
 
 // Read the JSON data from the file
-$file = (dirname(__FILE__).'/../storage/data/featured.json');
+$file = (dirname(__FILE__).'/../../storage/data/featured.json');
 $json_data = file_get_contents($file);
 
 // Decode the JSON data into a PHP array
@@ -208,7 +208,7 @@ $featured_3_id = $data['featured_3_id'];
     <div class="templates">
   <?php
 // Read the JSON data from the file
-$file = (dirname(__FILE__).'/../storage/data/articles.json');
+$file = (dirname(__FILE__).'/../../storage/data/articles.json');
 $json_data = file_get_contents($file);
 
 // Decode the JSON data into a PHP array
