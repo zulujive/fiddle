5e5a65ffdeb92167487021f70da6093aee79a775
zulujive <51298470+zulujive@users.noreply.github.com>
2023-06-02 17:27:22 +0000

    Redirect logged in users to panel

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+if ($_SESSION["logged_in"] == true) {
+    header("Location: /admin/panel/");
+}
+
 $valid_users = [
     "test" => "1234",
     // Add more authorized users here
