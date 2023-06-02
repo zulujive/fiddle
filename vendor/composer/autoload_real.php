0e4d35490ec12c3aa0d846c4b1243c20a3f46e3d
zulujive <51298470+zulujive@users.noreply.github.com>
2023-06-02 05:12:53 +0000

    Bye bye Sentinel (you won't be missed)

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInita03941a9c3a87d4083a34e08ff6f858d
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInita03941a9c3a87d4083a34e08ff6f858d', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInita03941a9c3a87d4083a34e08ff6f858d', 'loadClassLoader'));
@@ -32,18 +30,6 @@ class ComposerAutoloaderInita03941a9c3a87d4083a34e08ff6f858d
         call_user_func(\Composer\Autoload\ComposerStaticInita03941a9c3a87d4083a34e08ff6f858d::getInitializer($loader));
 
         $loader->register(true);
-
-        $filesToLoad = \Composer\Autoload\ComposerStaticInita03941a9c3a87d4083a34e08ff6f858d::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
-        }
 
         return $loader;
     }
