0b3777dba3dd77671e17bc44815d9e408d6d083a
zulujive <51298470+zulujive@users.noreply.github.com>
2023-06-02 16:46:33 +0000

    Changed login background

--- public/admin/index.php
+++ public/admin/index.php
@@ -18,7 +18,7 @@
 <body>
     <h1 class="text-center">PxlsFiddle Admin</h1>
     <br>
-    <form class="container card" method="get" action="index.php" style="width: 40%;"><br>
+    <form class="container card bg-primary" method="get" action="index.php" style="width: 40%;"><br>
         <div class="form-group">
             <label for="username-login">Username</label>
             <input id="username-login" class="form-control" type="text" name="username">
