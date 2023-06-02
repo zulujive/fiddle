d9df51c5784447af3e8ce3b8128f91f01545f49b
zulujive <51298470+zulujive@users.noreply.github.com>
2023-06-02 19:15:00 +0000

    Updated style controller again

--- src/controllers/StyleController.php
+++ src/controllers/StyleController.php
@@ -5,6 +5,6 @@ class StyleController
     public function index()
     {
         // Load the view
-        require_once __DIR__ . '/../resources/css';
+        require_once __DIR__ . '/../resources/css/';
     }
 }
