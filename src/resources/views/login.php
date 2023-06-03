11955e59ce632f47c970feea4955e655fecb6dca
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-03 19:57:59 +0000

    Used isset for undefined session variables

--- src/resources/views/login.php
+++ src/resources/views/login.php
@@ -1,11 +1,13 @@
 <?php
 session_start();
 
+if (!isset($_SESSION['logged_in'])) {
+    $_SESSION['logged_in'] = false;
+}
+
 if ($_SESSION["logged_in"] == true) {
     header("Location: /admin/panel/");
     exit();
-} else {
-    $_SESSION["logged_in"] = false;
 }
 
 $error_message = null;
