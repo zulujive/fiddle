6701a2e8cf1a0331bb56191152231644bef034af
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-04 19:25:51 +0000

    More margins

--- src/resources/views/login.php
+++ src/resources/views/login.php
@@ -75,7 +75,7 @@ if (!isset($_SESSION['csrf_token'])) {
             <label for="pwd-login">Password</label>
             <input id="pwd-login" class="form-control shadow" type="password" name="password" required>
         </div><br>
-        <button class="btn btn-light shadow mt-2" type="submit" value="submit" style="width: 20%;">Login</button><br>
+        <button class="btn btn-light shadow mt-3 mb-3" type="submit" value="submit" style="width: 20%;">Login</button><br>
     </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
