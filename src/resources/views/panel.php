155cfa60428350cfc76e7c758baa6ce7d9241ee9
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-04 05:40:41 +0000

    Added margins

--- src/resources/views/panel.php
+++ src/resources/views/panel.php
@@ -23,6 +23,12 @@ if ($_SESSION["logged_in"] !== true) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
     <title>FiddleAdmin</title>
+
+    <style>
+        body {
+            margin: 1rem;
+        }
+    </style>
 </head>
 <body>
     <br>
