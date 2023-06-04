8b4724e1ed4dcb02dd88acbc9109425464fada0a
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-04 04:30:49 +0000

    Centered text

--- src/resources/views/404.php
+++ src/resources/views/404.php
@@ -11,8 +11,8 @@
 </head>
 <body>
     <br>
-    <h1>404 - Page Not Found</h1><br>
-    <p>We're sorry, but the page you're looking for does not exist</p>
+    <h1 class="text-center">404 - Page Not Found</h1><br>
+    <p class="text-center">We're sorry, but the page you're looking for does not exist</p>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
