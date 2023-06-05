22a72bd8f8e6a6467a11cbeface3bfd195795843
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-05 19:28:25 +0000

    cloudflare work now?

--- src/middleware/AuthMiddleware.php
+++ src/middleware/AuthMiddleware.php
@@ -18,7 +18,7 @@ $bootstrapStyleHash = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/boo
 $bootstrapScriptHash = 'sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz';
 $cloudflareCdn = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/*';
 
-$cspHeader = "default-src 'self'; script-src 'self' '$bootstrapScriptHash'; style-src https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css $bootstrapStyleHash 'self' 'unsafe-inline'; img-src 'self'; font-src 'self' https://cdnjs.cloudflare.com/ajax/libs/font-awesome/*;";
+$cspHeader = "default-src 'self'; script-src 'self' '$bootstrapScriptHash'; style-src https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css $bootstrapStyleHash 'self' 'unsafe-inline'; img-src 'self'; font-src 'self' 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/*';";
 
 header("Content-Security-Policy: " . $cspHeader);
 
