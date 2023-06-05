5b85a475cb5b452e78b58cd359e6e54c3ee1e2ff
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-05 07:12:10 +0000

    fixed

--- src/middleware/AuthMiddleware.php
+++ src/middleware/AuthMiddleware.php
@@ -11,11 +11,11 @@
 ######################################################################
 */
 
-$bootstrapStyleHash = 'cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css';
+$bootstrapStyleHash = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css';
 $bootstrapScriptHash = 'sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz';
 $cloudflareCdn = 'https://cdnjs.cloudflare.com';
 
-$cspHeader = "default-src 'self'; script-src 'self' '$bootstrapScriptHash'; style-src '$bootstrapStyleHash' 'self' 'unsafe-inline' '$cloudflareCdn'; img-src 'self';";
+$cspHeader = "default-src 'self'; script-src 'self' '$bootstrapScriptHash'; style-src 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css' 'self' 'unsafe-inline' '$cloudflareCdn'; img-src 'self';";
 
 header("Content-Security-Policy: " . $cspHeader);
 
