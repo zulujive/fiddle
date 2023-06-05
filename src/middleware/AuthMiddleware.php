e2cc5e04339fa15df5ed2c5fc3bb7940220793cd
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-05 07:26:02 +0000

    work now?

--- src/middleware/AuthMiddleware.php
+++ src/middleware/AuthMiddleware.php
@@ -13,7 +13,7 @@
 
 $bootstrapStyleHash = 'cdn.jsdelivr.net';
 $bootstrapScriptHash = 'sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz';
-$cloudflareCdn = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/';
+$cloudflareCdn = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome';
 
 $cspHeader = "default-src 'self'; script-src 'self' '$bootstrapScriptHash'; style-src https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css 'self' 'unsafe-inline' $cloudflareCdn; img-src 'self';";
 
