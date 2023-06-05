6b19dafe68b7efed2025df136c7f55373f0a2492
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-05 02:46:14 +0000

    Added instructions to middleware file

--- public/index.php
+++ public/index.php
@@ -85,6 +85,9 @@ $router->get('/admin', function () {
     $controller = new AdminController();
     $controller->panel();
 });
+$router->get('/admin/e', function () {
+    echo "Hello World!";
+});
 
 // -------------------------------------
 
--- src/middleware/AuthMiddleware.php
+++ src/middleware/AuthMiddleware.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+######################################################################
+#                                                                    #
+#                       ----Middleware----                           #
+#  This file defines the pages that should be protected by           #
+#  authentication. Remember that anything here will be executed      #
+#  before any other routes.                                          #
+#                                                                    #
+######################################################################
+*/
+
 $router->before('GET|POST', '/admin', function() {
     if ($_SESSION['logged_in'] !== true) {
         header('Location: /login');
