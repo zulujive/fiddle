49bbf4008a0905051c312e275456194bf260b613
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-05 06:43:59 +0000

    Maybe hashing work now?

--- src/middleware/AuthMiddleware.php
+++ src/middleware/AuthMiddleware.php
@@ -11,8 +11,8 @@
 ######################################################################
 */
 
-$bootstrapStyleHash = 'sha256-d0b418ae4b7d91889127767575c9ce57aa715c6215ef293fa030cb55b3b408c1';
-$bootstrapScriptHash = 'sha256-432dcb740086c0389579a29ebdd4a6d5ec55238ed53df70710119b8ad03887fe';
+$bootstrapStyleHash = 'sha256-Ouvbu/f7aL3Jh8SEcFqeja+uD9vth575jdoeGeJiQl8=';
+$bootstrapScriptHash = 'sha256-qlPVgvl+tZTCpcxYJFdHB/m6mDe84wRr+l81VoYPTgQ=';
 $cloudflareCdn = 'https://cdnjs.cloudflare.com';
 
 $cspHeader = "default-src 'self'; script-src 'self' $bootstrapScriptHash; style-src 'self' $bootstrapStyleHash 'unsafe-inline'; img-src 'self';";
