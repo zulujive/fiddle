2c9a3c2b666577cd612bfd2c2a28b3a432697614
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-05 03:20:57 +0000

    Added navbar paths

--- src/resources/views/panel.php
+++ src/resources/views/panel.php
@@ -21,20 +21,20 @@
 <body>
     <nav class="navbar navbar-expand-lg mb-3 bg-body-tertiary border-bottom border-bottom-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Admin</a>
+        <a class="navbar-brand" href="/admin">Admin</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="/admin">Home</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="#">Templates</a>
+            <a class="nav-link" href="/admin/templates">Templates</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="#">Users</a>
+            <a class="nav-link" href="/admin/users">Users</a>
             </li>
         </ul>
         <form class="d-flex">
