74f5fbcf6f1b5d38252261a945abac75f3e268ec
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-05 18:46:39 +0000

    Fixed CSP

--- src/middleware/AuthMiddleware.php
+++ src/middleware/AuthMiddleware.php
@@ -19,8 +19,8 @@ $bootstrapStyleHash = 'cdn.jsdelivr.net';
 $bootstrapScriptHash = 'sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz';
 $cloudflareCdn = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/*';
 
-$cspHeader = "default-src 'self'; script-src 'self' '$bootstrapScriptHash'; style-src https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css
-; img-src 'self'; font-src 'self' https://cdnjs.cloudflare.com/ajax/libs/font-awesome/*;";
+$cspHeader = "default-src 'self'; script-src 'self' '$bootstrapScriptHash'; style-src 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+; img-src 'self'; font-src 'self' 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/*;'";
 
 header("Content-Security-Policy: " . $cspHeader);
 
