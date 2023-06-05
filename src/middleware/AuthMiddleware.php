e76587b5221a06ceec22ff60d810abb1a18d9811
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-05 06:40:00 +0000

    Hashed content

--- src/middleware/AuthMiddleware.php
+++ src/middleware/AuthMiddleware.php
@@ -11,10 +11,11 @@
 ######################################################################
 */
 
-$bootstrapCdn = 'https://cdn.jsdelivr.net';
+$bootstrapStyleHash = 'sha256-3aebdbbbf7fb68bdc987c484705a9e8dafae0fdbed879ef98dda1e19e262425f';
+$bootstrapScriptHash = 'sha256-aa53d582f97eb594c2a5cc5824574707f9ba9837bce3046bfa5f3556860f4e04';
 $cloudflareCdn = 'https://cdnjs.cloudflare.com';
 
-$cspHeader = "default-src 'self'; script-src 'self' $bootstrapCdn; style-src 'self' $bootstrapCdn 'unsafe-inline'; img-src 'self';";
+$cspHeader = "default-src 'self'; script-src 'self' $bootstrapScriptHash; style-src 'self' $bootstrapStyleHash 'unsafe-inline'; img-src 'self';";
 
 header("Content-Security-Policy: " . $cspHeader);
 
