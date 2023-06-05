83dd0374f7bafca02185c77c502214b185fec1f2
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-05 07:18:22 +0000

    work now?

--- src/middleware/AuthMiddleware.php
+++ src/middleware/AuthMiddleware.php
@@ -15,9 +15,9 @@ $bootstrapStyleHash = 'cdn.jsdelivr.net';
 $bootstrapScriptHash = 'sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz';
 $cloudflareCdn = 'https://cdnjs.cloudflare.com';
 
-$cspHeader = "default-src 'self'; script-src 'self' '$bootstrapScriptHash'; style-src 'cdn.jsdelivr.net' 'self' 'unsafe-inline' '$cloudflareCdn'; img-src 'self';";
+$cspHeader = "default-src 'self'; script-src 'self' '$bootstrapScriptHash'; style-src 'https://cdn.jsdelivr.net' 'self' 'unsafe-inline' '$cloudflareCdn'; img-src 'self';";
 
-//header("Content-Security-Policy: " . $cspHeader);
+header("Content-Security-Policy: " . $cspHeader);
 
 $router->before('GET|POST', '/admin', function() {
     if ($_SESSION['logged_in'] !== true) {
