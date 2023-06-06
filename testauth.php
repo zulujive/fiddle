f316d250d1f3fa16b67be4b7c8e2b55cd7454442
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-06 19:39:03 +0000

    imported class

--- testauth.php
+++ testauth.php
@@ -1,10 +1,12 @@
 <?php
 
+use GuzzleHttp\Client;
+
 // Make sure to include the HTTP client library you prefer to use
 // For example, Guzzle is a popular HTTP client for PHP
 
 // Make a POST request to the authentication endpoint
-$client = new \GuzzleHttp\Client();
+$client = new Client();
 $response = $client->post('http://127.0.0.1:8090/auth', [
     'json' => [
         'username' => 'test',
