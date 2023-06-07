e728782a13b33f59c4e5312ed770bc7ed480d4ab
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-07 22:43:50 +0000

    fills whole body maybe?

--- src/resources/views/users.php
+++ src/resources/views/users.php
@@ -30,8 +30,8 @@ if (!isset($_SESSION["registration_success"])) {
 
     <title>Users</title>
 </head>
-<body>
-<div class="container-fluid">
+<body class="d-flex">
+<div class="container-fluid flex-grow-1">
   <div class="row">
     <div class="col-lg-2 m-0 d-flex">
       <nav class="navbar navbar-dark bg-primary flex-column m-0 flex-grow-1" style="margin-left:-1rem !important">
