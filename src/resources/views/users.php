e62ba44dd7e8bfc0339ff73d3a33380083fc8e79
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-07 20:52:46 +0000

    Changed column sizing

--- src/resources/views/users.php
+++ src/resources/views/users.php
@@ -56,74 +56,74 @@ if (!isset($_SESSION["registration_success"])) {
         </div>
     </nav>
     <div class="m-4">
-    <div class="row mb-3">
-        <div class="col-sm-6 mb-3 mb-sm-0">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title text-center"><?php echo $adminCount ?> Staff</h5>
+        <div class="row mb-3">
+            <div class="col-sm-6 mb-3 mb-sm-0">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title text-center"><?php echo $adminCount ?> Staff</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title text-center"><?php echo $userCount ?> Users</h5>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-sm-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title text-center"><?php echo $userCount ?> Users</h5>
-                </div>
+        <div class="row gx-3">
+            <div class="col-sm-3">
+                <form class="card bg-dark text-white shadow p-3" method="post" action="/admin/register">
+                    <h3 class="text-center">
+                            Create Staff Account
+                    </h3>
+                    <?php 
+                        if ($error_message !== null) {
+                            $printed_error = '<div class="alert alert-warning mt-3 mb-0" role="alert">';
+                            $printed_error .= $error_message;
+                            $printed_error .= '</div>';
+                            echo $printed_error;
+                        }
+                    ?>
+                    <?php 
+                        if ($_SESSION["registration_success"] === true) {
+                            $success_message = '<div class="alert alert-success mt-3 mb-0" role="alert">';
+                            $success_message .= 'User created successfully!';
+                            $success_message .= '</div>';
+                            echo $success_message;
+                            $_SESSION["registration_success"] = null;
+                        }
+                    ?>
+                    <input type="hidden" name="csrf_token" value="<?php echo $csrfToken; ?>">
+                    <div class="form-group mt-3">
+                        <label for="username-login">Username</label>
+                        <input id="username-login" class="form-control bg-white text-black shadow" type="text" name="username" required>
+                    </div>
+                    <div class="form-group mt-3">
+                        <label for="pwd-login">Password</label>
+                        <input id="pwd-login" class="form-control bg-white text-black shadow" type="password" name="password" required>
+                    </div>
+                    <div class="form-group mt-3">
+                        <label for="pwd-confirm">Confirm Password</label>
+                        <input id="pwd-confirm" class="form-control bg-white text-black shadow" type="password" name="passwordConfirm" required>
+                    </div>
+                    <button class="btn btn-light shadow mt-3 mb-3 w-50" type="submit" value="submit">Register</button>
+                </form>
+            </div>
+            <div class="card col-sm-3 bg-dark text-white shadow p-3">
+                <h3>Admins</h3>
+                <ul class="list-group list-group-flush">
+                    <?php echo $adminList ?>
+                </ul>
+            </div>
+            <div class="card col-sm-3 bg-dark text-white shadow p-3">
+                <h3>Users</h3>
+                <ul class="list-group list-group-flush">
+                    <?php echo $userList ?>
+                </ul>
             </div>
         </div>
-    </div>
-    <div class="row gx-3">
-        <div class="col">
-            <form class="card bg-dark text-white shadow p-3" method="post" action="/admin/register">
-                <h3 class="text-center">
-                        Create Staff Account
-                </h3>
-                <?php 
-                    if ($error_message !== null) {
-                        $printed_error = '<div class="alert alert-warning mt-3 mb-0" role="alert">';
-                        $printed_error .= $error_message;
-                        $printed_error .= '</div>';
-                        echo $printed_error;
-                    }
-                ?>
-                <?php 
-                    if ($_SESSION["registration_success"] === true) {
-                        $success_message = '<div class="alert alert-success mt-3 mb-0" role="alert">';
-                        $success_message .= 'User created successfully!';
-                        $success_message .= '</div>';
-                        echo $success_message;
-                        $_SESSION["registration_success"] = null;
-                    }
-                ?>
-                <input type="hidden" name="csrf_token" value="<?php echo $csrfToken; ?>">
-                <div class="form-group mt-3">
-                    <label for="username-login">Username</label>
-                    <input id="username-login" class="form-control bg-white text-black shadow" type="text" name="username" required>
-                </div>
-                <div class="form-group mt-3">
-                    <label for="pwd-login">Password</label>
-                    <input id="pwd-login" class="form-control bg-white text-black shadow" type="password" name="password" required>
-                </div>
-                <div class="form-group mt-3">
-                    <label for="pwd-confirm">Confirm Password</label>
-                    <input id="pwd-confirm" class="form-control bg-white text-black shadow" type="password" name="passwordConfirm" required>
-                </div>
-                <button class="btn btn-light shadow mt-3 mb-3 w-50" type="submit" value="submit">Register</button>
-            </form>
-        </div>
-        <div class="card col bg-dark text-white shadow p-3">
-            <h3>Admins</h3>
-            <ul class="list-group list-group-flush">
-                <?php echo $adminList ?>
-            </ul>
-        </div>
-        <div class="card col bg-dark text-white shadow p-3">
-            <h3>Users</h3>
-            <ul class="list-group list-group-flush">
-                <?php echo $userList ?>
-            </ul>
-        </div>
-    </div>
     </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
