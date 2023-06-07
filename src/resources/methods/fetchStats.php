0f730a1eda01ccc75ced4d3943bd6a24ef32d16d
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-07 19:30:51 +0000

    Reverted fetchStats

--- src/resources/methods/fetchStats.php
+++ src/resources/methods/fetchStats.php
@@ -5,12 +5,17 @@ class fetchStats
 {
     public static function countAdmins()
     {
-        $client = new Client();
+        $client = new Client(['defaults' => ['exceptions' => false]]);
 
         $response = $client->get('http://127.0.0.1:8090/api/collections/admins/records');
 
-        $responseData = json_decode($response->getBody(), true);
-        return $responseData['totalItems'];
+        if ($response->getStatusCode() === 200) {
+            $responseData = json_decode($response->getBody(), true);
+            if (isset($responseData['totalItems'])) {
+                return $responseData['totalItems'];
+            }
+        }
 
+        return 0; // Default value if the totalItems key is not found or the response is not successful
     }
 }
