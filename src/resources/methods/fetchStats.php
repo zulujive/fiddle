1fd1a818d740f99ef4f9efa47fdee38dd0a14ef1
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-07 20:27:05 +0000

    Fixed HTML variable list

--- src/resources/methods/fetchStats.php
+++ src/resources/methods/fetchStats.php
@@ -56,10 +56,10 @@ class fetchStats
             foreach($responseData['items'] as $item) {
                 $username = $item['username'];
 
-                $html = '<li class="list-group-item">' . $username . '</li>';
+                $html[] = '<li class="list-group-item">' . $username . '</li>';
 
-                return $html;
             }
+            return $html;
         }
     }
 }
