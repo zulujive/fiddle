4ddadd39baa52b0d79ed5756d39988b322e3db80
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-07 18:57:35 +0000

    added margins around body

--- src/resources/views/users.php
+++ src/resources/views/users.php
@@ -48,44 +48,45 @@ if (!isset($_SESSION["registration_success"])) {
             </div>
         </div>
     </nav>
-    <br>
-    <form class="card bg-dark text-white shadow w-25 p-3" method="post" action="/admin/register">
-        <br>
-        <h3 class="text-center">
-                Create Moderator Account
-        </h3>
-        <?php 
-            if ($error_message !== null) {
-                $printed_error = '<div class="alert alert-warning mt-3 mb-0" role="alert">';
-                $printed_error .= $error_message;
-                $printed_error .= '</div>';
-                echo $printed_error;
-            }
-        ?>
-        <?php 
-            if ($_SESSION["registration_success"] === true) {
-                $success_message = '<div class="alert alert-success mt-3 mb-0" role="alert">';
-                $success_message .= 'User created successfully!';
-                $success_message .= '</div>';
-                echo $success_message;
-                $_SESSION["registration_success"] = null;
-            }
-        ?>
-        <input type="hidden" name="csrf_token" value="<?php echo $csrfToken; ?>">
-        <div class="form-group mt-3">
-            <label for="username-login">Username</label>
-            <input id="username-login" class="form-control bg-white text-black shadow" type="text" name="username" required>
-        </div>
-        <div class="form-group mt-3">
-            <label for="pwd-login">Password</label>
-            <input id="pwd-login" class="form-control bg-white text-black shadow" type="password" name="password" required>
-        </div>
-        <div class="form-group mt-3">
-            <label for="pwd-confirm">Confirm Password</label>
-            <input id="pwd-confirm" class="form-control bg-white text-black shadow" type="password" name="passwordConfirm" required>
-        </div>
-        <button class="btn btn-light shadow mt-3 mb-3 w-50" type="submit" value="submit">Register</button>
-    </form>
+    <div class="m-3">
+        <form class="card bg-dark text-white shadow w-25 p-3" method="post" action="/admin/register">
+            <br>
+            <h3 class="text-center">
+                    Create Moderator Account
+            </h3>
+            <?php 
+                if ($error_message !== null) {
+                    $printed_error = '<div class="alert alert-warning mt-3 mb-0" role="alert">';
+                    $printed_error .= $error_message;
+                    $printed_error .= '</div>';
+                    echo $printed_error;
+                }
+            ?>
+            <?php 
+                if ($_SESSION["registration_success"] === true) {
+                    $success_message = '<div class="alert alert-success mt-3 mb-0" role="alert">';
+                    $success_message .= 'User created successfully!';
+                    $success_message .= '</div>';
+                    echo $success_message;
+                    $_SESSION["registration_success"] = null;
+                }
+            ?>
+            <input type="hidden" name="csrf_token" value="<?php echo $csrfToken; ?>">
+            <div class="form-group mt-3">
+                <label for="username-login">Username</label>
+                <input id="username-login" class="form-control bg-white text-black shadow" type="text" name="username" required>
+            </div>
+            <div class="form-group mt-3">
+                <label for="pwd-login">Password</label>
+                <input id="pwd-login" class="form-control bg-white text-black shadow" type="password" name="password" required>
+            </div>
+            <div class="form-group mt-3">
+                <label for="pwd-confirm">Confirm Password</label>
+                <input id="pwd-confirm" class="form-control bg-white text-black shadow" type="password" name="passwordConfirm" required>
+            </div>
+            <button class="btn btn-light shadow mt-3 mb-3 w-50" type="submit" value="submit">Register</button>
+        </form>
+    </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
