4e87e6218cfc8578e83bcc15ad4090762efb8672
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-07 22:21:48 +0000

    updated vertical nav

--- src/resources/views/users.php
+++ src/resources/views/users.php
@@ -31,27 +31,25 @@ if (!isset($_SESSION["registration_success"])) {
     <title>Users</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark flex-column align-items-start">
-    <a class="navbar-brand" href="#">Logo</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Services</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Contact</a>
-            </li>
-        </ul>
-    </div>
+<nav class="navbar navbar-dark bg-dark flex-column">
+  <a class="navbar-brand" href="#">Logo</a>
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link active" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Chat</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Friends</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Servers</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Settings</a>
+    </li>
+  </ul>
 </nav>
 
     <div class="m-4">
