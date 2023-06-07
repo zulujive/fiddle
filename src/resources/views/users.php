b58a553629dcb91066aec685f981df8ad7df4229
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-07 18:55:00 +0000

    Wrong padding lol

--- src/resources/views/users.php
+++ src/resources/views/users.php
@@ -49,7 +49,7 @@ if (!isset($_SESSION["registration_success"])) {
         </div>
     </nav>
     <br>
-    <form class="container card bg-dark text-white shadow w-25" method="post" action="/admin/register">
+    <form class="container card bg-dark text-white shadow w-25 p-3" method="post" action="/admin/register">
         <br>
         <h2 class="text-center">
                 Create Moderator Account
@@ -84,7 +84,7 @@ if (!isset($_SESSION["registration_success"])) {
             <label for="pwd-confirm">Confirm Password</label>
             <input id="pwd-confirm" class="form-control bg-white text-black shadow" type="password" name="passwordConfirm" required>
         </div>
-        <button class="btn btn-light shadow mt-3 mb-3 w-50 p-3" type="submit" value="submit">Register</button>
+        <button class="btn btn-light shadow mt-3 mb-3 w-50" type="submit" value="submit">Register</button>
     </form>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
