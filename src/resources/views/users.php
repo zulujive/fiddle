b847cd4c5f7c34b61bf5e9ccb38208cff8ea1a7b
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-07 21:56:45 +0000

    Added overflow scrolling

--- src/resources/views/users.php
+++ src/resources/views/users.php
@@ -112,7 +112,7 @@ if (!isset($_SESSION["registration_success"])) {
         <div class="col-md-4 d-flex">
             <div class="card flex-fill p-3">
                 <h3 class="card-title">Staff</h3>
-                <div class="card-body">
+                <div class="card-body" style="max-height: 300px; overflow-y: auto;">
                     <ul class="list-group list-group-flush">
                         <?php echo $adminList ?>
                     </ul>
@@ -122,7 +122,7 @@ if (!isset($_SESSION["registration_success"])) {
         <div class="col-md-4 d-flex">
             <div class="card flex-fill p-3">
                 <h3 class="card-title">Recent Signups</h3>
-                <div class="card-body">
+                <div class="card-body" style="max-height: 300px; overflow-y: auto;">
                     <ul class="list-group list-group-flush">
                         <?php echo $userList ?>
                     </ul>
