19554785cf774581ea587e9c5493583505f4334f
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-08 18:20:35 +0000

    Added required classes

--- src/resources/views/generateQR.php
+++ src/resources/views/generateQR.php
@@ -1,5 +1,13 @@
 <?php
+use Endroid\QrCode\Color\Color;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelLow;
 use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Label\Label;
+use Endroid\QrCode\Logo\Logo;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+use Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\Writer\ValidationException;
 use OTPHP\TOTP;
 
 $otp = TOTP::create();
