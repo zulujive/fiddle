d105980f34d0b0e35d38962d9707bf2694896298
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-09 14:08:38 -0600

    Added token handler class

---
+++ src/resources/methods/tokenHandler.php
@@ -0,0 +1,57 @@
+<?php
+use GuzzleHttp\Client;
+
+$client = new Client(['defaults' => [ 'exceptions' => false ]] );
+require_once __DIR__ . '/../../../config.php';
+
+class tokenHandler
+{
+    public static function createToken($type, $enabled, $user)
+    {
+        global $client;
+        $uuid = uuid_create();
+        $response = $client->post('' . DB_HOST . '/api/collections/tokens/records', [
+            'json' => [
+                'token' => $uuid,
+                'valid' => $enabled,
+                'for' => $type,
+                'userID' => $user,
+            ]
+        ]);
+
+        if ($response->getStatusCode() === 200) {
+            $responseData = json_decode($response->getBody(), true);
+            $token = $responseData['token'];
+            return $token;
+        }
+    }
+    public static function disableToken($token) {
+        global $client;
+
+        $id = this::findToken($token);
+
+        $response = $client->patch('' . DB_HOST . '/api/collections/tokens/records/' . $id, [
+            'json' => [
+                'valid' => false,
+            ]
+        ]);
+
+        $responseData = json_decode($response->getBody(), true);
+        
+        $state = $responseData['valid'];
+        return $state;
+
+    }
+    protected static function findToken($token) {
+        global $client;
+
+        $response = $client->get('' . DB_HOST . '/api/collections/tokens/records?filter=(token=' . $token . ')');
+        if ($response->getStatusCode() === 200) {
+            $responseData = json_decode($response->getBody(), true);
+            $array = $responseData['items'];
+            $firstItem = $array[0];
+            $id = $firstItem['id'];
+            return $id;
+        }
+    }
+}
