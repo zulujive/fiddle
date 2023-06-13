324f327651207ce8d3d8f12aa6f8597283fbf80a
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-13 15:21:43 -0600

    Formatting :)

--- config.example.php
+++ config.example.php
@@ -14,18 +14,25 @@
 |  Setup a PocketBase database and fill out the following
 |  parameters. Ensure you have a secure password set.
 */
-// Put the host of the database configured here (no trailing slashes):
+//                 \/ \/ \/ \/ \/ \/ \/  Put URL here (no trailing slashes)
 define('DB_HOST', 'http://127.0.0.1:8090');
-// Put the database API key here:
+
+//                \/ \/ \/ Put database password here (default "password")
 define('DB_KEY', 'password');
+
 /*-------------------------------------------------------
 |                    --Rate Limiting--
 |  Here you'll define rate limiting to prevent resource
 |  usage and brute-force attacks (in minutes).
 */
+//      \/ Name \/    \/ Rate Limit (in minutes)
 define('LOGIN_LIMIT', 1);
 
-// Enable or disable maintenance mode
+/*-------------------------------------------------------
+|                 --Maintanance Mode--
+|  If you need to disable the website, simply enable this
+|  setting and edit message to your liking
+*/
 $maintenanceMode = false;
 $maintenanceMessage = "The website is currently undergoing maintenance. We'll be back soon!";
 
