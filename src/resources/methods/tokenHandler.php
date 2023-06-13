c66ed36fc6232238ccab0ad31d0ef05ccda0b88f
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-13 12:52:42 -0600

    Testing db password

--- src/resources/methods/tokenHandler.php
+++ src/resources/methods/tokenHandler.php
@@ -11,7 +11,7 @@ class tokenHandler
         $uuid = bin2hex(random_bytes(32));
         $response = $client->post(DB_HOST . '/api/collections/tokens/records', [
             'json' => [
-                'dbkey' => 'password',
+                'dbkey' => 'poopoo',
                 'token' => $uuid,
                 'valid' => $enabled,
                 'for' => $type,
