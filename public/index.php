bf03d642ce6141e12d549a0b11bf8156a49e009f
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-20 17:34:47 -0600

    added errorHandle function

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 require __DIR__ . '/../src/middleware/ErrorMiddleware.php';
 new errorHandler();
+errorHandler()->errorHandle();
 
 /*
 ######################################################################
