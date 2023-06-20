676068e59742ee173ace0b9d3c53e6e0dd8f9f1e
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-20 17:15:48 -0600

    added error handler

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+use Core\ErrorMiddleware\errorHandler;
+errorHandler::errorHandle();
+
 /*
 ######################################################################
 #                                                                    #
