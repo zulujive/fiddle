b3b3fda2993c0577e980989ee16e3a2fabd9454b
zulujive <6002ekul@gmail.com>
2023-06-19 19:32:16 -0600

    Removed old controllers

--- src/controllers/AdminController.php
+++ src/controllers/AdminController.php
@@ -5,12 +5,6 @@ class AdminController
     public function panel() {
         require_once __DIR__ . '/../resources/views/panel.php';
     }
-    public function panelLogin() {
-        require_once __DIR__ . '/../resources/views/login.php';
-    }
-    public function panelLogout() {
-        require_once __DIR__ . '/../resources/views/logout.php';
-    }
     public function templatePanel() {
         require_once __DIR__ . '/../resources/views/templates.php';
     }
@@ -19,20 +13,5 @@ class AdminController
     }
     public function newUser() {
         require_once __DIR__ . '/../resources/views/register.php';
-    }
-    public function OTP() {
-        require_once __DIR__ . '/../resources/views/dualFactor.php';
-    }
-    public function verifyOTP() {
-        require_once __DIR__ . '/../resources/views/dualFactorVerify.php';
-    }
-    public function enableOTP() {
-        require_once __DIR__ . '/../resources/views/dualFactorEnable.php';
-    }
-    public function deployOTP() {
-        require_once __DIR__ . '/../resources/views/dualFactorInfo.php';
-    }
-    public function generateQR() {
-        require_once __DIR__ . '/../resources/views/generateQR.php';
     }
 }
