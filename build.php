1cfb677e78f0e6fb5f7abd6c77c1a6a902d824cb
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-21 09:58:13 -0600

    Revert back to original

--- build.php
+++ build.php
@@ -5,28 +5,8 @@ $mainScript = 'public/index.php';
 $phar = new Phar($pharFile);
 $phar->startBuffering();
 
-// Add PHP files
-$phar->buildFromDirectory(__DIR__, '/^(?!(build\.php)).+\.(php|inc)$/');
-
-// Add non-PHP files from directories
-$directories = [
-    'src/storage/templates',
-    'src/resources/css',
-    'src/resources/js',
-];
-
-foreach ($directories as $directory) {
-    $iterator = new RecursiveIteratorIterator(
-        new RecursiveDirectoryIterator($directory),
-        RecursiveIteratorIterator::SELF_FIRST
-    );
-    foreach ($iterator as $file) {
-        if ($file->isFile() && !$file->isDir()) {
-            $relativePath = substr($file->getPathname(), strlen($directory) + 1);
-            $phar->addFile($file->getPathname(), $relativePath);
-        }
-    }
-}
+// Add files and directories to the Phar archive
+$phar->buildFromDirectory(__DIR__, '/^(?!(build\.php)).+/');
 
 // Set the main script for execution when the Phar is run
 $phar->setStub("#!/usr/bin/env php\n" . $phar->createDefaultStub($mainScript));
