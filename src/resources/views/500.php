c1ef28da63710ecaa769119a4f250929a7c1f0e4
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-20 21:40:33 -0600

    Fixed formatting

--- src/resources/views/500.php
+++ src/resources/views/500.php
@@ -3,13 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Internal Error</title>
+        <title>Internal Server Error</title>
         <meta name="description" content="Something went wrong on our end.">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     </head>
     <body class="p-3">
-        <h1 class="text-center"><em>fiddleneo</em> Something went wrong</h1>
+        <h1 class="text-center">Something went wrong :(</h1>
         <hr>
         <h4 class="text-center">Something went wrong on our end. Please contact a system administrator if you continue to have issues.</h4>
     </body>
