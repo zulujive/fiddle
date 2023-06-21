6941b056444774cc81d55ad52642aaf17917d29c
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-20 20:45:22 -0600

    Edited error handler for production

--- src/controllers/ErrorController.php
+++ src/controllers/ErrorController.php
@@ -14,16 +14,7 @@ class ErrorController
             $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
             $whoops->register();
         } else {
-            set_error_handler(function ($severity, $message, $file, $line) {
-                // Log the error
-                error_log("Error [$severity]: $message in $file on line $line");
-            
-                // Display the custom error page
-                echo 'Hello World!';
-            
-                // Stop script execution
-                exit();
-            });            
+            set_error_handler("Internal Error");            
         }
     }
 }
