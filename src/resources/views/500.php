47bd11988bb1a7eeb2a858baf0e4d7ffc2da5012
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-20 21:35:12 -0600

    Accidentally put fontawesome lol

--- src/resources/views/500.php
+++ src/resources/views/500.php
@@ -6,7 +6,7 @@
         <title>Internal Error</title>
         <meta name="description" content="Something went wrong on our end.">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     </head>
     <body class="p-3">
         <h1 class="text-center"><em>fiddleneo</em> Something went wrong</h1>
