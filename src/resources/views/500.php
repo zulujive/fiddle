c99e82f4c957a5295a05bf19c0d7739d6dcb8698
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-20 21:57:40 -0600

    Added text center globally

--- src/resources/views/500.php
+++ src/resources/views/500.php
@@ -8,11 +8,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     </head>
-    <body class="p-3">
-        <h1 class="text-center text-warning">500: Something went wrong :(</h1>
+    <body class="p-3 text-center">
+        <h1 class="text-warning">500: Something went wrong :(</h1>
         <hr>
-        <h4 class="text-center">Something went wrong on our end. Please contact a system administrator if you continue to have issues.</h4>
-        <small style="text-align: center;">If you are a developer, you can enable full debugging within the config file.</small>
+        <h4>Something went wrong on our end. Please contact a system administrator if you continue to have issues.</h4>
+        <small>If you are a developer, you can enable full debugging within the config file.</small>
     </body>
     <footer>
         <p class="text-center">fiddleneo</p>
