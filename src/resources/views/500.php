5fb4cad710c1ec3c81088c3d94fb5c2bf701e803
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-06-20 21:33:20 -0600

    removed arbitrary HTML

--- src/resources/views/500.php
+++ src/resources/views/500.php
@@ -1,8 +1,4 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
 <html>
     <head>
         <meta charset="utf-8">
@@ -15,8 +11,5 @@
     <body class="p-3">
         <h1 class="text-center"><em>fiddleneo</em> Something went wrong</h1>
         <h3 class="text-center">Something went wrong on our end. Please contact a system administrator if you continue to have issues.</h3>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
     </body>
 </html>
