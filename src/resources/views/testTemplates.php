c1c86e05945f7af591c9033e9ae7d53cc23717de
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-07-06 22:33:51 -0600

    Fixed undefined $client

--- src/resources/views/testTemplates.php
+++ src/resources/views/testTemplates.php
@@ -1,5 +1,8 @@
 <?php
 require __DIR__ . '/../methods/PocketBase.php';
+use GuzzleHttp\Client;
+
+$client = new Client();
 
 $imageTestUrl = PocketBase::getTemplateUrl('wrhjuujj7dwoe3x');
 $imageResponse = $client->get($imageTestUrl);
