fca043d11470586ddec92d7acd18ba6912920606
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-07-06 22:32:31 -0600

    testing serving image data

--- src/resources/views/testTemplates.php
+++ src/resources/views/testTemplates.php
@@ -2,4 +2,10 @@
 require __DIR__ . '/../methods/PocketBase.php';
 
 $imageTestUrl = PocketBase::getTemplateUrl('wrhjuujj7dwoe3x');
-echo $imageTestUrl;
+$imageResponse = $client->get($imageTestUrl);
+$imageData = $imageResponse->getBody()->getContents();
+
+// Set the appropriate content-type header for the image
+header('Content-Type: ' . $response->getHeaderLine('Content-Type'));
+
+echo $imageData;
