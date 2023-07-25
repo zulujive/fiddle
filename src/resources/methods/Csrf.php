7dba9c030cd2873a45129d34162d752341f7e33c
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-07-25 16:00:51 +0100

    Updated CSRF styling

--- src/resources/methods/Csrf.php
+++ src/resources/methods/Csrf.php
@@ -12,6 +12,9 @@ class Csrf
     {
         if ($_POST['csrf_token'] !== $_SESSION['csrf_token']) {
             http_response_code(400);
+            echo "<head>";
+            echo "<link href=\"https:\/\/cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM\" crossorigin=\"anonymous\">";
+            echo "</head>";
             echo "<body style=\"background-color: black; color: white; padding: 3rem;\">";
             echo "<h1>Error: 400</h1>";
             echo "<h3>Invalid CSRF token. Please enter the login page and try again.</h3>";
