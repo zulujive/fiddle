aa42a3f9c2d94e2f8ecc7b90254bf887013b3e95
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-07-25 11:26:39 +0100

    Regenerate token every time

--- src/resources/methods/Csrf.php
+++ src/resources/methods/Csrf.php
@@ -3,12 +3,9 @@ class Csrf
 {
     public static function generateToken()
     {
-        if (!isset($_SESSION['csrf_token'])) {
-            $csrfToken = bin2hex(random_bytes(32));
-            $_SESSION['csrf_token'] = $csrfToken;
-        } else {
-            $csrfToken = $_SESSION['csrf_token'];
-        }
+        $csrfToken = bin2hex(random_bytes(32));
+        $_SESSION['csrf_token'] = $csrfToken;
+        
         return $csrfToken;
     }
     public static function verifyToken()
