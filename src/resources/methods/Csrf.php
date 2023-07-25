cab79b449a5127cb2ba8432ea4a66e769075ecf2
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-07-25 11:29:38 +0100

    changed background to body not div

--- src/resources/methods/Csrf.php
+++ src/resources/methods/Csrf.php
@@ -5,17 +5,17 @@ class Csrf
     {
         $csrfToken = bin2hex(random_bytes(32));
         $_SESSION['csrf_token'] = $csrfToken;
-        
+
         return $csrfToken;
     }
     public static function verifyToken()
     {
         if ($_POST['csrf_token'] !== $_SESSION['csrf_token']) {
             http_response_code(400);
-            echo "<div style=\"background-color: black; color: white;\">";
+            echo "<body style=\"background-color: black; color: white;\">";
             echo "<h1>Error: 400</h1>";
             echo "<h3>Invalid CSRF token. Please enter the login page and try again.</h3>";
-            echo "</div>";
+            echo "</body>";
             exit();
         }
     }
