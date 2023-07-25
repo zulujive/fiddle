1db67ea9325b16d54e43ca5bd88fd1857391954a
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-07-25 22:19:50 +0100

    Added styling to template 404

--- src/resources/methods/PocketBase.php
+++ src/resources/methods/PocketBase.php
@@ -65,7 +65,13 @@ class PocketBase
             return $imageName;
         } else {
             http_response_code(404);
-            echo 'Cannot find template';
+            echo '<head>'
+            echo "<link href=\"https:\/\/cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM\" crossorigin=\"anonymous\">";
+            echo '</head>'
+            echo '<body>'
+            echo '<h1>Error: 404</h1>'
+            echo '<h3>Cannot find template<h3>';
+            echo '</body>'
             exit();
         }
     }
