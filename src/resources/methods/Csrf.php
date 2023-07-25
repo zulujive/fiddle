25c1cae9dccd547cf4eef9792835df098a1b7784
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-07-25 15:51:40 +0100

    Added padding to error

--- src/resources/methods/Csrf.php
+++ src/resources/methods/Csrf.php
@@ -12,7 +12,7 @@ class Csrf
     {
         if ($_POST['csrf_token'] !== $_SESSION['csrf_token']) {
             http_response_code(400);
-            echo "<body style=\"background-color: black; color: white;\">";
+            echo "<body style=\"background-color: black; color: white; padding: 3rem;\">";
             echo "<h1>Error: 400</h1>";
             echo "<h3>Invalid CSRF token. Please enter the login page and try again.</h3>";
             echo "</body>";
