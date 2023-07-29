bcdf765b4275c789c2e390c92384bfc3edd29033
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-07-29 19:27:25 +0100

    Update error styling

--- src/resources/methods/pb/PocketBaseUtils.php
+++ src/resources/methods/pb/PocketBaseUtils.php
@@ -67,13 +67,15 @@ class PocketBaseUtils
             return $imageName;
         } else {
             http_response_code(404);
+            echo '<html data-bs-theme"dark">';
             echo '<head>';
             echo "<link href=\"https:\/\/cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM\" crossorigin=\"anonymous\">";
             echo '</head>';
-            echo '<body class="p-3" style="background-color: black; color: white;">';
+            echo '<body class="p-3 text-center">';
             echo '<h1>Error: 404</h1>';
             echo '<h3>Cannot find template<h3>';
             echo '</body>';
+            echo '</html>'
             exit();
         }
     }
