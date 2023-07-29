676a5ffa483433cca651829520227db56cd94cdd
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-07-29 19:30:54 +0100

    Improved formatting

--- src/resources/methods/pb/PocketBaseUtils.php
+++ src/resources/methods/pb/PocketBaseUtils.php
@@ -72,8 +72,8 @@ class PocketBaseUtils
             echo "<link href=\"https:\/\/cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM\" crossorigin=\"anonymous\">";
             echo '</head>';
             echo '<body class="p-3 text-center">';
-            echo '<h1>Error: 404</h1>';
-            echo '<h3>Cannot find template<h3>';
+            echo '<h1>404 - Template Not Found</h1>';
+            echo '<h3>The template you\'re looking for cannot be found.<h3>';
             echo '</body>';
             echo '</html>';
             exit();
