2f27ce21b738dbd8428ebe680feb3384623aa10f
Luke Roszmann <51298470+zulujive@users.noreply.github.com>
2023-07-29 19:28:35 +0100

    "=" from hell

--- src/resources/methods/pb/PocketBaseUtils.php
+++ src/resources/methods/pb/PocketBaseUtils.php
@@ -67,7 +67,7 @@ class PocketBaseUtils
             return $imageName;
         } else {
             http_response_code(404);
-            echo '<html data-bs-theme"dark">';
+            echo '<html data-bs-theme="dark">';
             echo '<head>';
             echo "<link href=\"https:\/\/cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM\" crossorigin=\"anonymous\">";
             echo '</head>';
